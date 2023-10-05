3da543954de0e4bfab5143149fbcf3ab8e30075a
hnejman <108231655+hnejman@users.noreply.github.com>
2023-10-05 11:58:48 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,6 @@ export const App = () => {
   const [good, handleGood] = useState(0);
   const [neutral, handleNeutral] = useState(0);
   const [bad, handleBad] = useState(0);
-  const [total, handleTotal] = useState(0);
-
-
   
     return (
       <div>
@@ -18,18 +15,15 @@ export const App = () => {
             handleGood={handleGood}
             handleNeutral={handleNeutral}
             handleBad={handleBad}
-            handleTotal={handleTotal}
             good={good} 
             neutral={neutral}
             bad={bad}
-            total={total}
           />
           <h2>Statistics:</h2>
           <Statistics 
           good={good} 
           neutral={neutral}
           bad={bad}
-          total={total}
           />
       </div>
     );
